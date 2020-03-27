40426115dcd354f7506a6f5fbfaf7afb6f193088
Tabassum Hassan <tiram819@gmail.com>
2020-03-27 02:53:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Angram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(array)
+    match = []
+    some_word.split(" ").sort 
+  end
+    
+    
+end
