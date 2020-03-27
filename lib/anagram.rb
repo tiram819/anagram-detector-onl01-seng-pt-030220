2a3634443df64203e57967a90bd9e4c0100db80f
Tabassum Hassan <tiram819@gmail.com>
2020-03-27 02:57:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,8 @@ class Anagram
     @word = word 
   end
   
-  def match(word_array)
-    word_array.select do |word|
+  def match(some_word)
+    some_word.select do |word|
       word.split("").sort == @word.split("").sort
     end
   end
